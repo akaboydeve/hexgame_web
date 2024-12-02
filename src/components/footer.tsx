7c9e05246e7c3603a83409e1133b5941b30dcf8a
NHero <79276833+NHeroOP@users.noreply.github.com>
2024-12-02 12:06:07 +0530

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -12,7 +12,7 @@ const Footer = () => {
     { href: '/ShippingAndDelivery', label: 'Shipping' },
     { href: '/TermsAndConditions', label: 'Terms And Conditions' },
     { href: '/PrivacyPolicy', label: 'Privacy Policy' },
-    { href: '/Cancellation and Refunds', label: 'Refunds' },
+    { href: '/CancellationAndRefunds', label: 'Refunds' },
     { href: '/ContactUs', label: 'Contact Us' },
   ]
 
