da4f1848cbab9c2d75d328e15992dd42c82f685b
NHeroOP <neeravtheheroo@gmail.com>
2024-12-04 15:59:55 +0530

    Add db and setup and feed initial data

---
+++ src/appwrite/server/dbSetup.ts
@@ -0,0 +1,38 @@
+import { db } from "../name";
+import { createAdminClient } from "./config";
+import { 
+  createRankCollection,
+  createStaffCollection
+} from "./collections";
+import feedData from "./feedData";
+
+
+export default async function getOrCreateDB() {
+  const { databases } = await createAdminClient();
+  try {
+
+    await databases.get(db);
+    console.log("Database connected");
+    await feedData()
+  }
+  catch (error) {
+    try {
+      await databases.create(db, db);
+      console.log("Database created");
+
+      // Create collections
+      await Promise.all([
+        createStaffCollection(),
+        createRankCollection(),
+      ])
+      console.log("Collections created");
+      console.log("Database connected");
+      await feedData()
+    }
+    catch (error) {
+      console.log("Error creating database", error);
+    }
+  }
+
+  return databases;
+}
---
+++ src/appwrite/server/feedData.ts
@@ -0,0 +1,49 @@
+import { ID } from "node-appwrite";
+import { db, rankCollection, staffCollection } from "../name";
+import { createAdminClient } from "./config";
+import { initialStaff, ranks } from "@/mcinfo";
+
+export default async function feedData() {
+  // await createRanks();
+  await addStaff();
+}
+
+
+async function createRanks() {
+  try {
+    const { databases } = await createAdminClient();
+
+    for (const rank of ranks) {
+      await databases.createDocument(db, rankCollection, ID.unique(), {
+        name: rank.name,
+        price: rank.price,
+        image: rank.image,
+        features: rank.features,
+        commands: rank.commands,
+        weeklyRewards: rank.weeklyRewards,
+        specials: rank.specials || [],
+      });
+      console.log(`Rank ${rank.name} created`);
+    }
+  } catch (error) {
+    console.log("Error feeding rank data", error);
+  }
+}
+
+async function addStaff() {
+  try {
+    const { databases } = await createAdminClient();    
+
+    for (const staff of initialStaff) {
+      await databases.createDocument(db, staffCollection, ID.unique(), {
+        name: staff.name,
+        role: staff.role,
+        avatar: staff.avatar,
+      })
+    }
+    console.log("Staff data fed");
+    
+  } catch (error) {
+    console.log("Error feeding staff data");
+  }
+}
