e5721d972a505aa7c04aa1b26a8418a3ed4ed84e
NHeroOP <neeravtheheroo@gmail.com>
2024-12-04 15:57:01 +0530

    Add env file to easily acess env

---
+++ src/env.ts
@@ -0,0 +1,12 @@
+const env = {
+  appwrite: {
+    endpoint: String(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT),
+    projectId: String(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID),
+    apiKey: String(process.env.APPWRITE_API_KEY),
+  },
+  resend: {
+    apiKey: String(process.env.RESEND_API_KEY)
+  }
+}
+
+export default env;
