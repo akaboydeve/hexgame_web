1a47023c8fdb9a6b23b6d455de1b826e1d110642
NHeroOP <neeravtheheroo@gmail.com>
2024-12-04 19:02:08 +0530

    Update middelware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { getLoggedInUser } from './appwrite/server/config';
 
 const caseSensitiveRoutes = [
   '/PrivacyPolicy',
@@ -9,11 +10,20 @@ const caseSensitiveRoutes = [
   '/CancellationAndRefund',
 ];
 
-export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
+export async function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+  
+  const user = await getLoggedInUser()
+  
 
   if (pathname.startsWith('/api')) {
     return NextResponse.next();
+  }
+
+  const isLoginPage = pathname.toLowerCase() === "/login"
+
+  if (user && (isLoginPage)) {
+    return NextResponse.redirect(new URL("/store", req.nextUrl))
   }
 
   const correctCasePath = caseSensitiveRoutes.find(
@@ -21,7 +31,7 @@ export function middleware(request: NextRequest) {
   );
 
   if (correctCasePath && correctCasePath !== pathname) {
-    const url = request.nextUrl.clone();
+    const url = req.nextUrl.clone();
     url.pathname = correctCasePath;
     return NextResponse.redirect(url);
   }
@@ -30,5 +40,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/:path*'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
