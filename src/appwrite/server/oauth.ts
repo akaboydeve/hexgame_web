e15cdbf28b9979cce937a2e079d7a64c43a6f6f2
NHeroOP <neeravtheheroo@gmail.com>
2024-12-05 17:09:02 +0530

    Add hardcoded url

--- src/appwrite/server/oauth.ts
+++ src/appwrite/server/oauth.ts
@@ -1,17 +1,14 @@
 "use server"
 
 import { createAdminClient } from "./config";
-import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import { OAuthProvider } from "node-appwrite";
 
 export async function signInWithGoogle() {
   const { account } = await createAdminClient();
-
-  const origin = (await headers()).get("origin");
   
-  const successUrl = `${origin}/api/oauth`;
-  const failureUrl = `${origin}/login`;
+  const successUrl = `https://hexgame.in/api/oauth`;
+  const failureUrl = `https://hexgame.in/login`;
 
   const redirectUrl = await account.createOAuth2Token(
     OAuthProvider.Google,
