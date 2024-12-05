d058c42d773cda08cd30e351a3c4f4d64c1f73b1
NHeroOP <neeravtheheroo@gmail.com>
2024-12-05 12:24:19 +0530

    forgot to add await

--- src/app/api/send-details/route.ts
+++ src/app/api/send-details/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   try {
-    const user = getLoggedInUser()
+    const user = await getLoggedInUser()
     if (!user) {
       return NextResponse.json({success: false}, {status: 401});
     }
