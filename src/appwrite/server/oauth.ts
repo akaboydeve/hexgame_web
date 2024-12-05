15fcb2f28a2d4652c57f4783f55f526287f0f4ca
NHeroOP <neeravtheheroo@gmail.com>
2024-12-05 15:12:22 +0530

    Add console logs to check

--- src/appwrite/server/oauth.ts
+++ src/appwrite/server/oauth.ts
@@ -9,15 +9,20 @@ export async function signInWithGoogle() {
   const { account } = await createAdminClient();
 
   const origin = (await headers()).get("origin");
+  console.log("origin", origin);
   
   const successUrl = `${origin}/api/oauth`;
   const failureUrl = `${origin}/login`;
+  console.log("successUrl", successUrl);
+  console.log("failureUrl", failureUrl);
 
   const redirectUrl = await account.createOAuth2Token(
     OAuthProvider.Google,
     successUrl,
     failureUrl
   );
+
+  console.log("redirectUrl", redirectUrl);
 
   redirect(redirectUrl);
 }
